cf1d576fa7ce8acd40eb24bfd9328d880d99807a
Erik Zubia <ezubia@Eriks-MacBook-Pro.local>
2016-05-12 08:41:19 -0600

    Se cambia el puerto de salida.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 
 
 var app = express();
-var port = 8080;
+var port = 80;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
